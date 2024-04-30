87cc29c1ae549f03328e0cd8d0222805b4656999
Gustavo Tavares <gt3oliveira@gmail.com>
2024-04-30 14:00:01 -0300

    feat: add inital search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "./_components/header";
+import { Search } from "./_components/search";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
       <div className="flex w-full items-center p-8 text-2xl">
         Home Page - FullStack Week Four
       </div>
