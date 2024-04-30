13b44c4bbb8c705d53b154ce1eb6c4dbd7dc456d
Gustavo Tavares <gt3oliveira@gmail.com>
2024-04-30 13:45:34 -0300

    feat: add initial header components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Header } from "./_components/header";
 
 export default function Home() {
   return (
-    <div className="w-full text-2xl p-8 flex items-center">
-      Home Page - FullStack Week Four
-    </div>
-  )
+    <>
+      <Header />
+      <div className="flex w-full items-center p-8 text-2xl">
+        Home Page - FullStack Week Four
+      </div>
+    </>
+  );
 }
