564387e0c7ff2846f1243efffad7ce76cf47571f
Gustavo Tavares <gt3oliveira@gmail.com>
2024-05-01 20:36:31 -0300

    style: improve logo quality on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,11 +2,20 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export function Header() {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="FSW Foods" height={30} width={100} />
+      <Link href={"/"} className="relative h-[30px] w-[100px]">
+        <Image
+          src="/logo.png"
+          alt="FSW Foods"
+          fill
+          className="object-cover"
+          quality={100}
+        />
+      </Link>
       <Button
         size={"icon"}
         variant={"outline"}
