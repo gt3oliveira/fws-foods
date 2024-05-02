cff835d2d661710e9d237c94eae7241aa673a146
Gustavo Tavares <gt3oliveira@gmail.com>
2024-05-01 21:24:16 -0300

    feat: add recommended products page

---
+++ app/products/recommended/page.tsx
@@ -0,0 +1,40 @@
+import { Header } from "@/app/_components/header";
+import { ProductItem } from "@/app/_components/product-item";
+import { db } from "@/app/_lib/prisma";
+import React from "react";
+
+export default async function RecommendedProductsPage() {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 20,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="mb-6 px-5">
+        <h2 className="py-6 text-lg font-semibold">Pedidos Recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
